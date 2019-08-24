88fc579922042db5e735aba97baea41f5de19960
Kenneth Loeffler <kenneth.loeffler@outlook.com>
2019-08-23 19:07:19 -0700

    Add documentation, add replicator dirty flag

--- src/ComponentFactory.lua
+++ src/ComponentFactory.lua
@@ -1,4 +1,13 @@
 -- ComponentFactory.lua
+local CollectionService = game:GetService("CollectionService")
+local
+local Constants = require(scipt.Parent.EntityReplicator.Constants)
+local ReplicatorShared = require(script.Parent.EntityReplicator.Shared)
+
+local PARAMS_UPDATE = Constants.PARAMS_UPDATE
+local IS_SERVER = Constants.IS_SERVER
+
+local queue = ReplicatorShared.Queue
 
 local ComponentDesc = require(script.Parent.ComponentDesc)
 local WSAssert = require(script.Parent.WSAssert)
@@ -8,43 +17,66 @@ local ComponentMetatable = {
 		local paramId = ComponentDesc.GetParamIdFromName(component._componentId, index)
 		return component[paramId]
 	end,
+
 	__newindex = function(component, index, value)
 		local componentId = component._componentId
 		local paramId = ComponentDesc.GetParamIdFromName(componentId, index)
 		local ty = typeof(ComponentDesc.GetParamDefault(componentId, paramId))
+
 		WSAssert(ty == typeof(value), "expected %s", ty)
+
 		component[paramId] = value
+
+		if CollectionService:HasTag(instance, "__WSReplicatorRef") then
+			queue(component.Instance, PARAMS_UPDATE, componentId, paramId)
+		end
 	end
 }
 
----Instantiates a new component of type componentType on the entity attached to instance with parameters defined by paramMap
---@param instance
---@param componentType
---@param paramMap
+---Instantiates a new component of type componentType on the entity attached to instance with parameters defined by paramMap.
+
+-- If performance is a concern, then it is better for paramMap to be a contigous array (where each numeric index represents
+-- a paramId). This not only removes loop overhead, but also avoids the cost of rehashing paramMap when the numeric keys are
+-- inserted.
+
+-- To also avoid the two rehashes when assigning to ._componentId and .Instance, one may assign values to these same indices
+-- (the values don't particularly matter; the number 0 would suffice) when supplying paramMap to EntityManager.AddComponent().
+
+-- As with any optimization, only do this when absolutely necessary; it will heavily reduce readability. It may be helpful to
+-- include a comment in the system which indicates which parameter names go with which paramIds.
+
+--@param instance Instance
+--@param componentType string
+--@param paramMap table
 --@return The new component object
+
 function Component(instance, componentType, paramMap)
-	local newComponent = {} 
-
 	local componentId = ComponentDesc.GetComponentIdFromType(componentType)
+	local newComponent = paramMap
 
-	newComponent._componentId = componentId
-	newComponent.Instance = instance
-	if #paramMap == 0 then
+	if #newComponent == 0 then
 		for paramName, default in pairs(ComponentDesc.GetDefaults(componentId)) do
 			local paramId = ComponentDesc.GetParamIdFromName(componentId, paramName)
-			if paramMap[paramName] ~= nil then
-				newComponent[paramId] = paramMap[paramName]
+
+			if newComponent[paramName] ~= nil then
+				newComponent[paramId] = newComponent[paramName]
+				newComponent[paramName] = nil
 			else
-				newComponent[paramId] = default
+				paramMap[paramId] = default
 			end
-		end
-	else
-		for paramId, v in ipairs(paramMap) do
-			newComponent[paramId] = v
 		end
 	end
 
-	return setmetatable(newComponent, ComponentMetatable)
+	newComponent._componentId = componentId
+	newComponent.Instance = instance
+
+	setmetatable(newComponent, ComponentMetatable)
+
+	if CollectionService:HasTag(instance, "__WSReplicatorRef") then
+		queue(instance, ADD_COMPONENT, componentId)
+	end
+
+	return newComponent
 end
 
 return Component
