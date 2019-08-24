ca09546774405e9af87d5c7d044b8f78e2de7c60
Kenneth Loeffler <kenneth.loeffler@outlook.com>
2019-08-23 19:09:58 -0700

    Add method for getting number of parameters per component

--- src/ComponentDesc.lua
+++ src/ComponentDesc.lua
@@ -4,6 +4,7 @@ local ComponentIdsByType = {}
 local ComponentTypesById = {}
 local ComponentParamIds = {}
 local Defaults = {}
+local NumComponentParams = {}
 
 local ComponentDesc = {
 	ComponentDefinitions = {},
@@ -15,36 +16,44 @@ local function populateDefs(definitionTable)
 
 		WSAssert(typeof(componentType) == "string", "expected string")
 		WSAssert(typeof(componentDefinition) == "table", "expected table")
-	
+
 		local componentId = componentDefinition.ComponentId
 		WSAssert(componentId ~= nil and typeof(componentId) == "number" and math.floor(componentId) == componentId, "expected number")
 		ComponentIdsByType[componentType] = componentId
 		ComponentTypesById[componentId] = componentType
 		ComponentParamIds[componentId] = {}
 		Defaults[componentId] = {}
-	
+		NumComponentParams[componentId] = 0
+
 		for paramName, paramDef in pairs(componentDefinition) do
 			if paramName ~= "ComponentId" then
 				WSAssert(typeof(paramName) == "string", "expected string")
 				WSAssert(typeof(paramDef) == "table", "expected table")
 				ComponentParamIds[componentId][paramName] = paramDef[1]
 				Defaults[componentId][paramName] = paramDef[2]
+				NumComponentParams[componentId] = NumComponentParams[componentId] +	1
 			end
 		end
 	end
+
 	ComponentDesc.ComponentDefinitions = definitionTable
 end
 
-if script:WaitForChild("ComponentDefinitions", 2) then 
+ComponentDesc.NumParamsByComponentId = NumParamsByComponentId
+
+if script:WaitForChild("ComponentDefinitions", 2) then
 	local componentDefinitions = require(script.ComponentDefinitions)
+
 	populateDefs(componentDefinitions)
 	print(script:GetFullName())
+
 	script.ComponentDefinitions:GetPropertyChangedSignal("Source"):Connect(function()
 		populateDefs(require(script.ComponentDefinitions:Clone()))
+
 		if ComponentDesc._defUpdateCallback then
 			ComponentDesc._defUpdateCallback()
 		end
-	end)	
+	end)
 end
 
 function ComponentDesc.GetDefaults(componentId)
@@ -80,4 +89,3 @@ function ComponentDesc.GetAllComponents()
 end
 
 return ComponentDesc
-
