2f9556073d232cb4ded1d3f73b42ae5b52d243fd
Kenneth Loeffler <kenneth.loeffler@outlook.com>
2019-08-23 22:07:50 -0700

    Fix styling

--- src/WSAssert.lua
+++ src/WSAssert.lua
@@ -1,14 +1,17 @@
 function WSAssert(condition, ...)
 	if not condition then
 		local var = {...}
+
 		if next(var) then
 			local success, msg = pcall(function()
 				return string.format(unpack(var))
 			end)
+
 			if success then
 				error("WorldSmith: assertion failed: " .. msg, 3)
 			end
 		end
+
 		error("WorldSmith: assertion failed", 3)
 	end
 end
