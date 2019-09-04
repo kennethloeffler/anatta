a7a2905822078f78711c15c0c06c131a3ddb2be8
Kenneth Loeffler <kenneth.loeffler@outlook.com>
2019-09-04 12:25:59 -0700

    Implement rest of client

--- src/EntityReplicator/Client.lua
+++ src/EntityReplicator/Client.lua
@@ -1,51 +1,93 @@
 local Workspace = game:GetService("Workspace")
 
 local ComponentDesc = require(script.Parent.Parent.ComponentDesc)
+local Constants = require(script.Parent.Constants)
 local Shared = require(script.Parent.Shared)
 
 local Client = {}
 
-local NetworkIdsByInstance = {}
-local InstancesByNetworkId = {}
-local EntityManager
-local entityMap
-local componentMap
+local SendBuffer = {}
 
-local deserializeNext = Shared.DeserializeNext
-local getIdStringFromNum = Shared.GetIdStringFromNum
+local Queued = Shared.Queued
+local DeserializeNext = Shared.DeserializeNext
+local GetIdStringFromNum = Shared.GetIdStringFromNum
 
----Deserializes the
+local RemoteEvent
+local RemoteFunction
+
 local function deserializePrefab(rootInstance, entities, ...)
-	for _, v in ipairs(rootInstance._s:GetChildren()) do
-		local entitiesIndex = 1
-		local paramsIndex = 0
-		local params =  { ... }
-		local networkId, componentIdsToParams, isReferenced
+	local entitiesIndex = 1
+	local paramsIndex = 1
+	local params = table.pack(...)
 
-		networkId, componentIdsToParams, entitiesIndex, paramsIndex = deserializeNext(entities, params, entitiesIndex, paramsIndex)
-
-		local instance = entitiesFolder[getIdStringFromNum(networkId)]
-		instance.Name = "__WSEntity"
-
-		for componentId, params in pairs(componentIdsToParams) do
-			Client.EntityManager.AddComponent(instance, ComponentDesc.GetComponentTypeFromId(componentId), params)
-		end
+	for _ in next, entities do
+		entitiesIndex, paramsIndex = DeserializeNext(entities, params, entitiesIndex, paramsIndex, rootInstance)
 	end
 
 	rootInstance.Parent = Workspace
 end
 
-function Client.Init(entityManager, entityMap, componentMap)
+local function deserializeUnique(rootInstance, entities, ...)
+	local entitiesIndex = 1
+	local paramsIndex = 1
+	local params = table.pack(...)
+
+	for _ in next, entities do
+		entitiesIndex, paramsIndex = DeserializeNext(entities, params, entitiesIndex, paramsIndex, rootInstance)
+	end
+
+	rootInstance.Parent = Workspace
+end
+
+function Client.SendAddComponent(component)
+	QueueUpdate(instance, ADD_COMPONENT, component._componentId)
+end
+
+function Client.SendParameterUpdate(component, paramName)
+	QueueUpdate(instance, PARAMS_UPATE, component._componentId, GetParamIdFromName(paramsName))
+end
+
+function Client.Step()
+	if next(Queued) then
+		local entities = SendBuffer[1]
+		local params = SendBuffer[2]
+		local entitiesIndex = 1
+		local paramsIndex = 1
+
+		for instance, msgMap in pairs(Queued) do
+			paramsIndex, entitiesIndex = SerializeNext(
+				entities, params,
+				entitiesIndex, paramsIndex,
+				msgMap
+			)
+
+			Queued[instance] = nil
+		end
+
+		RemoteEvent:FireServer(entities, table.unpack(params))
+
+		for i in ipairs(entities) do
+			entities[i] = nil
+		end
+
+		for i in ipairs(params) do
+			params[i] = nil
+		end
+	end
+end
+
+function Client.Init()
 	RemoteEvent = Players.LocalPlayer.PlayerGui:WaitForChild("RemoteEvent")
 	RemoteFunction = Players.LocalPlayer.PlayerGui:WaitForChild("RemoteFunction")
 
-	Shared.Init(entityMap, componentMap)
+	RemoteEvent.OnServerEvent:Connect(function(rootInstance, entities, ...)
+		local entitiesIndex = 1
+		local paramsIndex = 1
+		local params = table.pack(...)
 
-	EntityManager = entityManager
-	entityMap = entityMap
-	componentMap = componentMap
-
-	RemoteEvent.OnServerEvent:Connect(function()
+		for _ in next, entities do
+			entitiesIndex, paramsIndex = DeserializeNext(entities, params, entitiesIndex, paramsIndex, rootInstance)
+		end
 	end)
 
 	local function onServerInvoke(rootInstance, entitiesStruct, ...)
@@ -67,3 +109,4 @@ function Client.Init(entityManager, entityMap, componentMap)
 end
 
 return Client
+
