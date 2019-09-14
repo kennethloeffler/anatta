988036ab93fbb3eec00046e07f40e1c2db1042c1
kennethloeffler <kenneth.loeffler@outlook.com>
2019-09-14 02:52:04 -0700

    Revert "Make empty tables prettier"
    Inserted tabs between curly braces :fearful:
    
    This reverts commit 916bab8c596c8e2e76f48d8497f304c69dc55aeb.

--- plugin/Serial.lua
+++ plugin/Serial.lua
@@ -45,7 +45,7 @@ function Serial.SerializeValue(data, depth)
 			str[#str + 1] = string.format("%s  %s = %s,", indent, safeKey, Serial.SerializeValue(value, depth + 1))
 		end
 
-		str[#str + 1] = indent.."}"
+		str[#str + 1] = next(data) and indent.."}" or "}"
 	elseif ty == "Vector2" then
 		str = string.format("Vector2.new(%f, %f)", data.X, data.Y)
 	elseif ty == "Vector3" then
