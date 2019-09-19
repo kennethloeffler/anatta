ffa86693215364a73e4e3867f1b2f17d6ab9616e
Kenneth Loeffler <kenneth.loeffler@outlook.com>
2019-09-18 22:10:14 -0700

    Make entity system (including replicator) a module

---
+++ src/init.lua
@@ -0,0 +1,47 @@
+local Constants = require(script.Constants)
+local EntityManager = require(script.EntityManager)
+local EntityReplicator = Constants.IS_SERVER or Constants.IS_CLIENT and require(script.EntityReplicator)
+
+return {
+	-- EntityManager
+	AddComponent = EntityManager.AddComponent,
+	GetComponent = EntityManager.GetComponent,
+	GetAllComponentsOfType = EntityManager.GetAllComponentsOfType,
+
+	ComponentAdded = EntityManager.ComponentAdded,
+	ComponentKilled = EntityManager.ComponentKilled,
+
+	FilteredEntityAdded = EntityManager.FilteredEntityAdded,
+	FilteredEntityRemoved = EntityManager.FilteredEntityRemoved,
+
+	KillComponent = EntityManager.KillComponent,
+	KillEntity = EntityManager.KillEntity,
+
+	LoadSystem = EntityManager.LoadSystem,
+	UnloadSystem = EntityManager.UnloadSystem,
+	StartSystems = EntityManager.StartSystems,
+	StopSystems = EntityManager.StopSystems,
+
+	Init = EntityManager.Init,
+	Destroy = EntityManager.Destroy,
+
+	-- EntityReplicator
+	SendAddComponent = EntityReplicator and EntityReplicator.SendAddComponent,
+	SendParameterUpdate = EntityReplicator and EntityReplicator.SendParameterUpdate,
+
+	Reference = EntityReplicator and EntityReplicator.Reference,
+	Dereference = EntityReplicator and EntityReplicator.Dereference,
+
+	ReferenceGlobal = EntityReplicator and EntityReplicator.ReferenceGlobal,
+	DereferenceGlobal = EntityReplicator and EntityReplicator.DereferenceGlobal,
+
+	ReferenceForPrefab = EntityReplicator and EntityReplicator.ReferenceForPrefab,
+	DereferenceForPrefab = EntityReplicator and EntityReplicator.DereferenceForPrefab,
+
+	ReferenceForPlayer = EntityReplicator and EntityReplicator.ReferenceForPlayer,
+	DereferenceForPlayer = EntityReplicator and EntityReplicator.DereferenceForPlayer,
+
+	Unique = EntityReplicator and EntityReplicator.Unique,
+	UniqueFromPrefab = EntityReplicator and EntityReplicator.UniqueFromPrefab,
+}
+
