51a456c43106d37cc99890463a83a0c8aa344ac2
Kenneth Loeffler <kenneth.loeffler@outlook.com>
2019-09-18 22:11:15 -0700

    Add assertions

--- src/Component.lua
+++ src/Component.lua
@@ -1,7 +1,12 @@
+local WSAssert = require(script.Parent.WSAssert)
+
 -- Component.lua
 local Component = {}
 
 function Component.Define(componentTypeName, paramMap)
+	WSAssert(typeof(componentTypeName) == "string", "bad argument #1 (expected string)")
+	WSAssert(typeof(paramMap) == "table", "bad argument #2 (expected table)")
+
 	return { componentTypeName, paramMap }
 end
 
