2ee0f00ea6f9eda67252452634c7a48b192deb7b
Kenneth Loeffler <kennethloeffler@outlook.com>
2019-10-04 17:36:18 -0700

    Make ComponentLabel and ParamField list-typed

--- plugin/PluginComponents/ParamField.lua
+++ plugin/PluginComponents/ParamField.lua
@@ -1,7 +1,7 @@
 local root = script.Parent.Parent.Parent
 local Component = require(root.src.Component)
 
-return Component.Define("ParamField", {
+return Component.Define({"ParamField"}, {
 	ComponentType = "none",
 	ParamId = 0,
 	ParamValue = false, -- variant
