6ccd6e265a12f37244bac487bb9684f81e09dafe
Kenneth Loeffler <kennethloeffler@outlook.com>
2019-10-14 18:46:32 -0700

    Change table .Component to string .ComponentType

--- plugin/PluginComponents/SerializeKillComponent.lua
+++ plugin/PluginComponents/SerializeKillComponent.lua
@@ -2,5 +2,5 @@ local root = script.Parent.Parent.Parent
 local Component = require(root.src.Component)
 
 return Component.Define("SerializeKillComponent", {
-	Component = {} -- variant componentType
+	ComponentType = "none"
 })
