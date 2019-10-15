6519b040e8664b24da84f080cc6d5c3b6222c637
Kenneth Loeffler <kennethloeffler@outlook.com>
2019-10-14 18:45:57 -0700

    Add missing definitions

--- plugin/PluginComponents/SerializeDeleteGameComponent.lua
+++ plugin/PluginComponents/SerializeDeleteGameComponent.lua
@@ -1,1 +1,7 @@
-return
+local root = script.Parent.Parent.Parent
+local Component = require(root.src.Component)
+
+return Component.Define("SerializeDeleteGameComponent", {
+    ComponentType = "none"
+})
+
