98b17d9b42aa41838bda3d355124b50ef847e6c2
Kenneth Loeffler <kennethloeffler@outlook.com>
2020-01-05 09:10:59 -0800

    Rename Component.Define -> DefineNetworked

--- src/Component.lua
+++ src/Component.lua
@@ -29,7 +29,7 @@ function Component.DefineLocal(componentTypeName, paramMap)
 	return { componentTypeName, paramMap, true }
 end
 
-function Component.Define(componentTypeName, paramMap)
+function Component.DefineNetworked(componentTypeName, paramMap)
 	WSAssert(typeof(componentTypeName) == "string" or (typeof(componentTypeName) == "table" and typeof(componentTypeName[1]) == "string"), "bad argument #1 (expected string)")
 	WSAssert(typeof(paramMap) == "table", "bad argument #2 (expected table)")
 
