0dbd7102d9d826db60ab270479e3d15c755309b0
Kenneth Loeffler <kenneth.loeffler@outlook.com>
2020-04-24 16:07:26 -0700

    Finish implementing identifiers

--- src/core/Ident.lua
+++ src/core/Ident.lua
@@ -7,35 +7,85 @@ Utility to associate names with numeric identifiers
 local IS_STUDIO = game:GetService("RunService"):IsStudio()
 
 local ATTRIBUTES_ENABLED = pcall(function()
-	script:GetAttributes()
+	return not not script:GetAttributes()
 end)
 
-local Ident = {}
+local PERSISTENT_MASK = 0xFFFF
 
+local Identify = {}
 local lookup = {}
 
--- begin at 1 for runtime identifiers so that they can map properly to
--- indices of array-like tables
 local runtimeMax = 1
-local persistentMax = 0
+local persistentMax = 1
 
+local ErrDNE = "No identifier exists for %s"
 
---[[
+if ATTRIBUTES_ENABLED then
+	for name, id in pairs(script:GetAttributes()) do
+		if type(id) == "number" then
+			lookup[name] = id
+			persistentMax = persistentMax + 1
 
- Generate an identifier guaranteed to be the same for all future runs
+			script:SetAttribute(name, nil)
+		end
+	end
+else
+	for _, obj in ipairs(script:GetChildren()) do
+		if typeof(obj) == "IntValue" then
+			lookup[obj.Name] = obj.Value
+			persistentMax = persistentMax + 1
 
-]]
-function Ident.GeneratePersistent(name)
-	assert(IS_STUDIO, "This function can only exectued from Roblox Studio")
+			obj:Destroy()
+		end
+	end
 end
 
 --[[
 
- Generate an identifier guaranteed to be the same for this run
+ Generate an identifier guaranteed to be the same for this run and all
+ future runs
 
 ]]
-function Ident.GenerateRuntime(name)
-	lookup[name] = runtimeMax + 1
+function Identify.GeneratePersistent(name)
+	assert(IS_STUDIO, "This function may only exectued in Roblox Studio")
+
+	if not lookup[name]
+	or bit32.band(lookup[name], PERSISTENT_MASK) == 0 then
+		if ATTRIBUTES_ENABLED then
+			script:SetAttribute(name, persistentMax)
+		else
+			local v = Instance.new("IntValue")
+
+			v.Name = name
+			v.Value = persistentMax
+			v.Parent = script
+		end
+
+		lookup[name] = lookup[name]
+			and bit32.bor(persistentMax, lookup[name])
+			or persistentMax
+
+		persistentMax = persistentMax + 1
+	end
+
+	return lookup[name]
+end
+
+--[[
+
+ Generate for this name an identifier not guaranteed to be the same
+ each run
+
+]]
+function Identify.GenerateRuntime(name)
+	assert(not lookup[name] or
+			  bit32.rshift(lookup[name], 16) == 0,
+		  "This name is already associated with a runtime identifier")
+
+	lookup[name] = lookup[name]
+		and bit32.bor(bit32.lshift(runtimeMax, 16), lookup[name])
+		or bit32.lshift(runtimeMax, 16)
+
 	runtimeMax = runtimeMax + 1
 
 	return runtimeMax
@@ -43,21 +93,32 @@ end
 
 --[[
 
- Specify an alias for an existing name
+ Change the name of an identifier that has already been named
 
 ]]
-function Ident.Alias(nameToAlias, alias)
+function Identify.Rename(oldName, newName)
+	assert(lookup[oldName], ErrDNE:format(oldName))
+
+	lookup[newName] = lookup[oldName]
+	lookup[oldName] = nil
 end
 
-function Ident.GetPersistent(object)
+function Identify.Persistent(object)
+	local identifier = lookup[tostring(object)]
+
+	if identifier then
+		return bit32.band(identifier, PERSISTENT_MASK)
+	end
+
+	error(ErrDNE:format(tostring(object)))
 end
 
-function Ident.GetRuntime(object)
+function Identify.Runtime(object)
 	local identifier = lookup[tostring(object)]
 
 	if identifier then
 		return bit32.rshift(identifier, 16)
 	end
 
-	error(("No identifier exists for %s"):format(tostring(object)))
+	error(ErrDNE:format(tostring(object)))
 end
