72f887769d719d0a423a529a727e93ba6b026b0c
Kenneth Loeffler <kenneth.loeffler@outlook.com>
2020-04-28 16:22:53 -0700

    Implement Snapshot::Entities and Snapshot::Destroyed

--- src/ecs/Snapshot.lua
+++ src/ecs/Snapshot.lua
@@ -1,11 +1,49 @@
 local Snapshot = {}
 Snapshot.__index = Snapshot
 
-function Snapshot.new(source, init)
+function Snapshot.new(source, lastDestroyed, getNextDestroyed)
 	return setmetatable({
 		Source = source,
-		InitEntity = init
+		LastDestroyed = lastDestroyed,
+		GetNextDestroyed = getNextDestroyed
 	}, Snapshot)
+end
+
+
+function Snapshot:Entities(destination)
+	local manifest = self.Source
+
+	destination:Size(manifest:NumEntities())
+
+	manifest:ForEach(function(entity)
+		destination:Entity(entity)
+	end)
+
+	return self
+end
+
+function Snapshot:Destroyed(destination)
+	local manifest = self.Source
+	local numDestroyed  = manifest.Size - manifest:NumEntities()
+	local getNext = self.GetNextDestroyed
+	local curr
+
+	destination:Size(numDestroyed)
+
+	if numDestroyed > 0 then
+		curr = self.LastDestroyed
+		destination:Destroyed(curr)
+
+		for _ = 1, numDestroyed do
+			curr = getNext(curr)
+			destination:Destroyed(curr)
+		end
+	end
+
+	return self
+end
+
+function Snapshot:Components(destination)
 end
 
 return Snapshot
