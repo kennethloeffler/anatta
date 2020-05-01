087f8cff023bef5a29cc6015fac73d4f2be23849
Kenneth Loeffler <kenneth.loeffler@outlook.com>
2020-05-01 16:17:55 -0700

    Implement Snapshot::Components

--- src/ecs/Snapshot.lua
+++ src/ecs/Snapshot.lua
@@ -6,44 +6,57 @@ function Snapshot.new(source, lastDestroyed, getNextDestroyed)
 		Source = source,
 		LastDestroyed = lastDestroyed,
 		GetNextDestroyed = getNextDestroyed
-	}, Snapshot)
+					}, Snapshot)
 end
 
-
-function Snapshot:Entities(destination)
+function Snapshot:Entities(container)
 	local manifest = self.Source
 
-	destination:Size(manifest:NumEntities())
+	container:Size(manifest:NumEntities())
 
 	manifest:ForEach(function(entity)
-		destination:Entity(entity)
+		container:Entity(entity)
 	end)
 
 	return self
 end
 
-function Snapshot:Destroyed(destination)
+function Snapshot:Destroyed(container)
 	local manifest = self.Source
 	local numDestroyed  = manifest.Size - manifest:NumEntities()
 	local getNext = self.GetNextDestroyed
 	local curr
 
-	destination:Size(numDestroyed)
+	container:Size(numDestroyed)
 
 	if numDestroyed > 0 then
 		curr = self.LastDestroyed
-		destination:Destroyed(curr)
+		container:Destroyed(curr)
 
 		for _ = 1, numDestroyed do
 			curr = getNext(curr)
-			destination:Entity(curr)
+			container:Entity(curr)
 		end
 	end
 
 	return self
 end
 
-function Snapshot:Components(destination)
+function Snapshot:Components(container, ...)
+	local manifest = self.Source
+	local instances
+	local serialize
+
+	for num, componentId in ipairs({ ... }) do
+		instances = manifest.Pools[componentId].Objects
+		serialize = container.Serial[num]
+
+		for index, entity in ipairs(manifest.Pools[componentId].Internal) do
+			serialize(container, entity, instances[index])
+		end
+	end
+
+	return self
 end
 
 return Snapshot
