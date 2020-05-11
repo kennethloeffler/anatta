fc4e0a082812a0223193607c66ba39de3fac857e
kennethloeffler <kenneth.loeffler@outlook.com>
2020-05-11 15:47:41 -0700

    Update test

--- src/core/Identify.spec.lua
+++ src/core/Identify.spec.lua
@@ -1,14 +1,13 @@
 return function()
 	local Identify = require(script.Parent.Identify)
-
-	Identify.Purge()
+	local ident = Identify.new("test")
 
 	describe("GenerateRuntime", function()
 		it("should generate a sequence", function()
 			local lastId
 
 			for i = 1, 5 do
-				local id = Identify.GenerateRuntime(string.char(i))
+				local id = ident:generateRuntime(string.char(i))
 
 				expect(id).to.equal(lastId and lastId + 1 or id)
 				lastId = id
@@ -18,13 +17,13 @@ return function()
 
 	describe("Runtime", function()
 		it("should throw if the name is not associated with an identifier", function()
-			expect(pcall(Identify.Runtime, game:GetService("HttpService"):GenerateGUID(false))).to.never.equal(true)
+			expect(pcall(ident.runtime, ident, string.char(6))).to.never.equal(true)
 		end)
 
 		it("should return the correct identifier", function()
-			local id = Identify.GenerateRuntime(string.char(6))
+			local id = ident:generateRuntime(string.char(6))
 
-			expect(id).to.equal(Identify.Runtime(string.char(6)))
+			expect(id).to.equal(ident:runtime(string.char(6)))
 		end)
 	end)
 end
