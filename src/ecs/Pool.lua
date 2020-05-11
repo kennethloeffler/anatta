6f31caa079b82cd9f3e71f962a19440b99b6dc77
kennethloeffler <kenneth.loeffler@outlook.com>
2020-05-11 15:50:55 -0700

    Change naming convention

--- src/ecs/Pool.lua
+++ src/ecs/Pool.lua
@@ -1,56 +1,60 @@
 local Signal = require(script.Parent.Parent.core.Signal)
 local SparseSet = require(script.Parent.SparseSet)
 
-local remove = SparseSet.Remove
-local insert = SparseSet.Insert
-local has = SparseSet.Has
+local remove = SparseSet.remove
+local insert = SparseSet.insert
+local has = SparseSet.has
 
 local Pool = {}
 
-Pool.Has = has
+Pool.has = has
 
 function Pool.new(dataType, capacity)
 	local pool = SparseSet.new(capacity)
 
 	if dataType then
-		pool.Objects = {} -- table.create(capacity or 0)
-		pool.Type = dataType
+		pool.objects = {} -- table.create(capacity or 0)
+		pool.type = dataType
 	end
 
-	pool.OnAssign = Signal.new()
-	pool.OnRemove = Signal.new()
-	pool.OnUpdate = Signal.new()
+	pool.onAssign = Signal.new()
+	pool.onRemove = Signal.new()
+	pool.onUpdate = Signal.new()
 
 	return pool
 end
 
-function Pool.Get(pool, entity)
+function Pool.get(pool, entity)
 	local index = has(pool, entity)
-	local objects = pool.Objects
+	local objects = pool.objects
 
 	if objects and index then
 		return objects[index]
 	end
 end
 
-function Pool.Assign(pool, entity, object)
-	insert(pool, entity)
+function Pool.assign(pool, entity, object)
+	local size = insert(pool, entity)
 
-	if pool.Objects then
-		table.insert(pool.Objects, object)
+	if pool.objects then
+		pool.objects[size] = object
 
 		return object
 	end
 end
 
-function Pool.Destroy(pool, entity)
-	local objects = pool.Objects
-	local size = pool.Size - 1
+function Pool.destroy(pool, entity)
+	local objects = pool.objects
+	local size = pool.size
 	local index = remove(pool, entity)
 
 	-- component type could be empty
 	if objects then
-		objects[index] = size ~= 0 and table.remove(objects) or nil
+		if size > 1 then
+			objects[index] = objects[size]
+		end
+
+		objects[size] = nil
 	end
 end
 
