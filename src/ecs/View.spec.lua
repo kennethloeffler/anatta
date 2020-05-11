1b07bed8a39d496f61ac3ee1071a740518f50e32
kennethloeffler <kenneth.loeffler@outlook.com>
2020-05-11 15:52:12 -0700

    Update tests

--- src/ecs/View.spec.lua
+++ src/ecs/View.spec.lua
@@ -1,68 +1,66 @@
 local View = require(script.Parent.View)
-local Identify = require(script.Parent.Parent.core.Identify)
 local Manifest = require(script.Parent.Manifest)
-local manifest = Manifest.new()
-
-Identify.Purge()
-
-local Component1 = manifest:Define("Test1", "table")
-local Component2 = manifest:Define("Test2", "table")
-local Component3 = manifest:Define("Test3", "table")
-
-local Pool1 = Manifest._getPool(manifest, Component1)
-local Pool2 = Manifest._getPool(manifest, Component2)
-local Pool3 = Manifest._getPool(manifest, Component3)
-
-for i = 1, 100 do
-	local entity = manifest:Create()
-
-	if i % 2 == 0 then
-		manifest:Assign(entity, Component1, {})
-	end
-
-	if i % 4 == 0 then
-		manifest:Assign(entity, Component2, {})
-	end
-
-	if i % 8 == 0 then
-		manifest:Assign(entity, Component3, {})
-	end
-end
 
 return function()
+	local manifest = Manifest.new()
+
+	local Component1 = manifest:define("Test1", "table")
+	local Component2 = manifest:define("Test2", "table")
+	local Component3 = manifest:define("Test3", "table")
+
+	local Pool1 = Manifest._getPool(manifest, Component1)
+	local Pool2 = Manifest._getPool(manifest, Component2)
+	local Pool3 = Manifest._getPool(manifest, Component3)
+
+	for i = 1, 100 do
+		local entity = manifest:create()
+
+		if i % 2 == 0 then
+			manifest:assign(entity, Component1, {})
+		end
+
+		if i % 4 == 0 then
+			manifest:assign(entity, Component2, {})
+		end
+
+		if i % 8 == 0 then
+			manifest:assign(entity, Component3, {})
+		end
+	end
+
 	describe("new", function()
 		it("should construct a single-component view when there is one included component and no excluded components", function()
 			local view = View.new({ Pool1 })
 
 			expect(getmetatable(view)).to.equal(View._singleMt)
-			expect(view.Included).to.equal(Pool1)
-			expect(view.Excluded).to.never.be.ok()
+			expect(view.included).to.equal(Pool1)
+			expect(view.excluded).to.never.be.ok()
 		end)
 
 		it("should construct a single-component view with exclusion list when there is one included component and one or more excluded components", function()
 			local view = View.new({ Pool1 }, { Pool3 })
 
 			expect(getmetatable(view)).to.equal(View._singleWithExclMt)
-			expect(view.Included).to.equal(Pool1)
-			expect(view.Excluded[1]).to.equal(Pool3)
+			expect(view.included).to.equal(Pool1)
+			expect(view.excluded[1]).to.equal(Pool3)
 		end)
 
 		it("should construct a multi-component view when there are multiple included components and no excluded components", function()
 			local view = View.new({ Pool1, Pool2 })
 
 			expect(getmetatable(view)).to.equal(View._multiMt)
-			expect(view.Included[1]).to.equal(Pool1)
-			expect(view.Included[2]).to.equal(Pool2)
-			expect(view.Excluded).to.never.be.ok()
+			expect(view.included[1]).to.equal(Pool1)
+			expect(view.included[2]).to.equal(Pool2)
+			expect(view.excluded).to.never.be.ok()
 		end)
 
 		it("should construct a multi-component view with exclusion list when there are multiple included components and one or more excluded components", function()
 			local view = View.new({ Pool1, Pool2 }, { Pool3 })
 
 			expect(getmetatable(view)).to.equal(View._multiWithExclMt)
-			expect(view.Included[1]).to.equal(Pool1)
-			expect(view.Included[2]).to.equal(Pool2)
-			expect(view.Excluded[1]).to.equal(Pool3)
+			expect(view.included[1]).to.equal(Pool1)
+			expect(view.included[2]).to.equal(Pool2)
+			expect(view.excluded[1]).to.equal(Pool3)
 		end)
 	end)
 
@@ -71,13 +69,13 @@ return function()
 		local entitiesToIterate = {}
 
 		it("should iterate all entities with at least the specified components", function()
-			for _, entity in ipairs(Pool3.Internal) do
-				if Pool1.External[entity] and Pool2.External[entity] then
+			for _, entity in ipairs(Pool3.internal) do
+				if Pool1.external[entity] and Pool2.external[entity] then
 					entitiesToIterate[entity] = true
 				end
 			end
 
-			view:ForEach(function(entity)
+			view:forEach(function(entity)
 				expect(entitiesToIterate[entity]).to.be.ok()
 			end)
 		end)
@@ -85,22 +83,22 @@ return function()
 		it("should pass component instances in the order given at the view's construction", function()
 			entitiesToIterate = {}
 
-			for _, entity in ipairs(Pool3.Internal) do
-				if Pool1.External[entity] and Pool2.External[entity] then
+			for _, entity in ipairs(Pool3.internal) do
+				if Pool1.external[entity] and Pool2.external[entity] then
 					entitiesToIterate[entity] = true
 				end
 			end
 
-			view:ForEach(function(entity, first, second, third)
+			view:forEach(function(entity, first, second, third)
 				expect(entitiesToIterate[entity]).to.be.ok()
 
-				expect(manifest:Get(entity, Component1)).to.be.ok()
-				expect(manifest:Get(entity, Component2)).to.be.ok()
-				expect(manifest:Get(entity, Component3)).to.be.ok()
+				expect(manifest:get(entity, Component1)).to.be.ok()
+				expect(manifest:get(entity, Component2)).to.be.ok()
+				expect(manifest:get(entity, Component3)).to.be.ok()
 
-				expect(first).to.equal(manifest:Get(entity, Component1))
-				expect(second).to.equal(manifest:Get(entity, Component2))
-				expect(third).to.equal(manifest:Get(entity, Component3))
+				expect(first).to.equal(manifest:get(entity, Component1))
+				expect(second).to.equal(manifest:get(entity, Component2))
+				expect(third).to.equal(manifest:get(entity, Component3))
 			end)
 		end)
 	end)
@@ -110,13 +108,13 @@ return function()
 		local entitiesToIterate = {}
 
 		it("should iterate all entities with at least the specified components", function()
-			for _, entity in ipairs(Pool3.Internal) do
-				if Pool1.External[entity] and Pool2.External[entity] then
+			for _, entity in ipairs(Pool3.internal) do
+				if Pool1.external[entity] and Pool2.external[entity] then
 					entitiesToIterate[entity] = true
 				end
 			end
 
-			view:ForEach(function(entity)
+			view:forEach(function(entity)
 				expect(entitiesToIterate[entity]).to.be.ok()
 			end)
 		end)
@@ -126,19 +124,19 @@ return function()
 		local view = View.new( { Pool1, Pool2, Pool3 })
 
 		it("should return true if the entity is iterated by the view", function()
-			local entity = manifest:Create()
+			local entity = manifest:create()
 
-			manifest:Assign(entity, Component1, {})
-			manifest:Assign(entity, Component2, {})
-			manifest:Assign(entity, Component3, {})
+			manifest:assign(entity, Component1, {})
+			manifest:assign(entity, Component2, {})
+			manifest:assign(entity, Component3, {})
 
-			expect(view:Has(entity)).to.equal(true)
+			expect(view:has(entity)).to.equal(true)
 		end)
 
 		it("should return false if the entity is not iterated by the view", function()
-			local entity = manifest:Create()
+			local entity = manifest:create()
 
-			expect(view:Has(entity)).to.equal(false)
+			expect(view:has(entity)).to.equal(false)
 		end)
 	end)
 
@@ -147,11 +145,11 @@ return function()
 		local entitiesToIterate = {}
 
 		it("should iterate all the entities with at least the specified component", function()
-			for _, entity in ipairs(Pool1.Internal) do
+			for _, entity in ipairs(Pool1.internal) do
 				entitiesToIterate[entity] = true
 			end
 
-			view:ForEach(function(entity)
+			view:forEach(function(entity)
 				expect(entitiesToIterate[entity]).to.be.ok()
 			end)
 		end)
@@ -159,14 +157,14 @@ return function()
 		it("should pass the correct component instance", function()
 			entitiesToIterate = {}
 
-			for _, entity in ipairs(Pool1.Internal) do
+			for _, entity in ipairs(Pool1.internal) do
 				entitiesToIterate[entity] = true
 			end
 
-			view:ForEach(function(entity, component)
+			view:forEach(function(entity, component)
 				expect(entitiesToIterate[entity]).to.be.ok()
-				expect(manifest:Has(entity, Component1)).to.be.ok()
-				expect(manifest:Get(entity, Component1)).to.equal(component)
+				expect(manifest:has(entity, Component1)).to.be.ok()
+				expect(manifest:get(entity, Component1)).to.equal(component)
 			end)
 		end)
 	end)
@@ -176,11 +174,11 @@ return function()
 		local entitiesToIterate = {}
 
 		it("should iterate all the entities with at least the specified component", function()
-			for _, entity in ipairs(Pool1.Internal) do
+			for _, entity in ipairs(Pool1.internal) do
 				entitiesToIterate[entity] = true
 			end
 
-			view:ForEachEntity(function(entity)
+			view:forEachEntity(function(entity)
 				expect(entitiesToIterate[entity]).to.be.ok()
 			end)
 		end)
@@ -192,8 +190,8 @@ return function()
 		it("should iterate all instances of the specified component", function()
 			local index = 1
 
-			view:ForEachComponent(function(component)
-				expect(Pool1.Objects[index]).to.equal(component)
+			view:forEachComponent(function(component)
+				expect(Pool1.objects[index]).to.equal(component)
 				index = index + 1
 			end)
 		end)
@@ -204,13 +202,13 @@ return function()
 		local entitiesToIterate = {}
 
 		it("should iterate all the entities with at least the included components and none of the excluded components", function()
-			for _, entity in ipairs(Pool2.Internal) do
-				if Pool1.External[entity] and not Pool3.External[entity] then
+			for _, entity in ipairs(Pool2.internal) do
+				if Pool1.external[entity] and not Pool3.external[entity] then
 					entitiesToIterate[entity] = true
 				end
 			end
 
-			view:ForEach(function(entity)
+			view:forEach(function(entity)
 				expect(entitiesToIterate[entity]).to.be.ok()
 			end)
 		end)
@@ -218,20 +216,20 @@ return function()
 		it("should pass component instances in the order given at the view's construction", function()
 			entitiesToIterate = {}
 
-			for _, entity in ipairs(Pool2.Internal) do
-				if Pool1.External[entity] and not Pool3.External[entity] then
+			for _, entity in ipairs(Pool2.internal) do
+				if Pool1.external[entity] and not Pool3.external[entity] then
 					entitiesToIterate[entity] = true
 				end
 			end
 
-			view:ForEach(function(entity, component2, component1)
+			view:forEach(function(entity, component2, component1)
 				expect(entitiesToIterate[entity]).to.be.ok()
 
-				expect(manifest:Get(entity, Component1)).to.be.ok()
-				expect(manifest:Get(entity, Component2)).to.be.ok()
+				expect(manifest:get(entity, Component1)).to.be.ok()
+				expect(manifest:get(entity, Component2)).to.be.ok()
 
-				expect(component1).to.equal(manifest:Get(entity, Component1))
-				expect(component2).to.equal(manifest:Get(entity, Component2))
+				expect(component1).to.equal(manifest:get(entity, Component1))
+				expect(component2).to.equal(manifest:get(entity, Component2))
 			end)
 		end)
 	end)
@@ -241,13 +239,13 @@ return function()
 		local entitiesToIterate = {}
 
 		it("should iterate all the entities with at least the included components and none of the excluded components", function()
-			for _, entity in ipairs(Pool2.Internal) do
-				if Pool1.External[entity] and not Pool3.External[entity] then
+			for _, entity in ipairs(Pool2.internal) do
+				if Pool1.external[entity] and not Pool3.external[entity] then
 					entitiesToIterate[entity] = true
 				end
 			end
 
-			view:ForEach(function(entity)
+			view:forEach(function(entity)
 				expect(entitiesToIterate[entity]).to.be.ok()
 			end)
 		end)
@@ -257,27 +255,27 @@ return function()
 		local view = View.new({ Pool1, Pool2 }, { Pool3 })
 
 		it("should return true if the entity is iterated by the view", function()
-			local entity = manifest:Create()
+			local entity = manifest:create()
 
-			manifest:Assign(entity, Component1, {})
-			manifest:Assign(entity, Component2, {})
+			manifest:assign(entity, Component1, {})
+			manifest:assign(entity, Component2, {})
 
-			expect(view:Has(entity)).to.equal(true)
+			expect(view:has(entity)).to.equal(true)
 		end)
 
 		it("should return false if the entity is not iterated by the view", function()
-			local entity1 = manifest:Create()
-			local entity2 = manifest:Create()
+			local entity1 = manifest:create()
+			local entity2 = manifest:create()
 
-			manifest:Assign(entity1, Component1, {})
-			manifest:Assign(entity1, Component2, {})
-			manifest:Assign(entity1, Component3, {})
+			manifest:assign(entity1, Component1, {})
+			manifest:assign(entity1, Component2, {})
+			manifest:assign(entity1, Component3, {})
 
-			manifest:Assign(entity2, Component2, {})
-			manifest:Assign(entity2, Component3, {})
+			manifest:assign(entity2, Component2, {})
+			manifest:assign(entity2, Component3, {})
 
-			expect(view:Has(entity1)).to.equal(false)
-			expect(view:Has(entity2)).to.equal(false)
+			expect(view:has(entity1)).to.equal(false)
+			expect(view:has(entity2)).to.equal(false)
 		end)
 	end)
 
@@ -286,13 +284,13 @@ return function()
 		local entitiesToIterate = {}
 
 		it("should iterate all the entities with at least the included component and none of the excluded components", function()
-			for _, entity in ipairs(Pool1.Internal) do
-				if not Pool2.External[entity] and not Pool3.External[entity] then
+			for _, entity in ipairs(Pool1.internal) do
+				if not Pool2.external[entity] and not Pool3.external[entity] then
 					entitiesToIterate[entity] = true
 				end
 			end
 
-			view:ForEach(function(entity)
+			view:forEach(function(entity)
 				expect(entitiesToIterate[entity]).to.be.ok()
 			end)
 		end)
@@ -300,16 +298,16 @@ return function()
 		it("should pass the correct component instance", function()
 			entitiesToIterate = {}
 
-			for _, entity in ipairs(Pool1.Internal) do
-				if not Pool2.External[entity] and not Pool3.External[entity] then
+			for _, entity in ipairs(Pool1.internal) do
+				if not Pool2.external[entity] and not Pool3.external[entity] then
 					entitiesToIterate[entity] = true
 				end
 			end
 
-			view:ForEach(function(entity, component)
+			view:forEach(function(entity, component)
 				expect(entitiesToIterate[entity]).to.be.ok()
-				expect(manifest:Has(entity, Component1)).to.be.ok()
-				expect(manifest:Get(entity, Component1)).to.equal(component)
+				expect(manifest:has(entity, Component1)).to.be.ok()
+				expect(manifest:get(entity, Component1)).to.equal(component)
 			end)
 		end)
 	end)
@@ -319,13 +317,13 @@ return function()
 		local entitiesToIterate = {}
 
 		it("should iterate all the entities with at least the included component and none of the excluded components", function()
-			for _, entity in ipairs(Pool1.Internal) do
-				if not Pool2.External[entity] and not Pool3.External[entity] then
+			for _, entity in ipairs(Pool1.internal) do
+				if not Pool2.external[entity] and not Pool3.external[entity] then
 					entitiesToIterate[entity] = true
 				end
 			end
 
-			view:ForEachEntity(function(entity)
+			view:forEachEntity(function(entity)
 				expect(entitiesToIterate[entity]).to.be.ok()
 			end)
 		end)
@@ -336,13 +334,13 @@ return function()
 		local entitiesToIterate = {}
 
 		it("should iterate all instances of the included component which do not belong to an entity with any of the excluded components", function()
-			for _, entity in ipairs(Pool1.Internal) do
-				if not Pool2.External[entity] and not Pool3.External[entity] then
+			for _, entity in ipairs(Pool1.internal) do
+				if not Pool2.external[entity] and not Pool3.external[entity] then
 					entitiesToIterate[entity] = true
 				end
 			end
 
-			view:ForEachComponent(function(entity)
+			view:forEachComponent(function(entity)
 				expect(entitiesToIterate[entity]).to.be.ok()
 			end)
 		end)
@@ -357,18 +355,18 @@ return function()
 
 	describe("doesntHaveExcluded", function()
 		it("should return true if the entity has none of the specified components", function()
-			local entity = manifest:Create()
+			local entity = manifest:create()
 
-			manifest:Assign(entity, Component1, {})
+			manifest:assign(entity, Component1, {})
 
 			expect(View._doesntHaveExcluded(entity, { Pool2, Pool3 })).to.equal(true)
 		end)
 
 		it("should return false if the entity has any of the specified components", function()
-			local entity = manifest:Create()
+			local entity = manifest:create()
 
-			manifest:Assign(entity, Component1, {})
-			manifest:Assign(entity, Component3, {})
+			manifest:assign(entity, Component1, {})
+			manifest:assign(entity, Component3, {})
 
 			expect(View._doesntHaveExcluded(entity, { Pool1, Pool3 })).to.equal(false)
 		end)
@@ -376,20 +374,20 @@ return function()
 
 	describe("hasIncluded", function()
 		it("should return false if the entity doesn't have all of the specified components", function()
-			local entity = manifest:Create()
+			local entity = manifest:create()
 
-			manifest:Assign(entity, Component1, {})
-			manifest:Assign(entity, Component3, {})
+			manifest:assign(entity, Component1, {})
+			manifest:assign(entity, Component3, {})
 
 			expect(View._hasIncluded(entity, { Pool1, Pool2 })).to.equal(false)
 		end)
 
 		it("should return true if the entity has all of the specified components", function()
-			local entity = manifest:Create()
+			local entity = manifest:create()
 
-			manifest:Assign(entity, Component1, {})
-			manifest:Assign(entity, Component2, {})
-			manifest:Assign(entity, Component3, {})
+			manifest:assign(entity, Component1, {})
+			manifest:assign(entity, Component2, {})
+			manifest:assign(entity, Component3, {})
 
 			expect(View._hasIncluded(entity, { Pool1, Pool2, Pool3 })).to.equal(true)
 		end)
@@ -397,12 +395,12 @@ return function()
 
 	describe("hasIncludedThenPack", function()
 		it("should return true and correctly populate the component pack if the entity has all of the specified components", function()
-			local entity = manifest:Create()
+			local entity = manifest:create()
 			local componentPack = {}
 
-			local first = manifest:Assign(entity, Component1, {})
-			local second = manifest:Assign(entity, Component2, {})
-			local third = manifest:Assign(entity, Component3, {})
+			local first = manifest:assign(entity, Component1, {})
+			local second = manifest:assign(entity, Component2, {})
+			local third = manifest:assign(entity, Component3, {})
 
 			expect(View._hasIncludedThenPack(entity, { Pool1, Pool2, Pool3 }, componentPack)).to.equal(true)
 			expect(componentPack[1]).to.equal(first)
@@ -411,11 +409,11 @@ return function()
 		end)
 
 		it("should return false if the entity doesn't have all of the specified components", function()
-			local entity = manifest:Create()
+			local entity = manifest:create()
 			local componentPack = {}
 
-			manifest:Assign(entity, Component1, {})
-			manifest:Assign(entity, Component3, {})
+			manifest:assign(entity, Component1, {})
+			manifest:assign(entity, Component3, {})
 
 			expect(View._hasIncludedThenPack(entity, { Pool1, Pool2, Pool3 }, componentPack)).to.equal(false)
 		end)
