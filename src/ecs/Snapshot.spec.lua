bd01135ff109839d2cc2df08d8f67bcc63064f7b
kennethloeffler <kenneth.loeffler@outlook.com>
2020-05-17 19:08:46 -0700

    Update test

--- src/ecs/Snapshot.spec.lua
+++ src/ecs/Snapshot.spec.lua
@@ -1,8 +1,4 @@
 local Manifest = require(script.Parent.Manifest)
-local Constants = require(script.Parent.Parent.Constants)
-
-local ENTITYID_MASK = Constants.ENTITYID_MASK
-local ENTITYID_WIDTH = Constants.ENTITYID_WIDTH
 
 return function()
 	local manifest = Manifest.new()
@@ -23,7 +19,6 @@ return function()
 		end
 
 		ents[ent] = true
-		destEnts[manifest:create()] = true
 	end
 
 	for entity in pairs(destEnts) do
@@ -37,54 +32,33 @@ return function()
 			expect(snapshot.source).to.equal(manifest)
 
 			expect(snapshot.entities).to.be.a("function")
-			expect(snapshot.destroyed).to.be.a("function")
 			expect(snapshot.components).to.be.a("function")
 		end)
 	end)
 
 	describe("Entities", function()
-		local snapshot = manifest:snapshot()
 		local container = {}
 
-		snapshot:entities(container)
+		manifest:snapshot():entities(container)
 
-		it("should serialize all of the entities alive", function()
-			for _, entity in ipairs(container[1]) do
-				expect(ents[entity]).to.be.ok()
-			end
-		end)
-	end)
-
-	describe("Destroyed", function()
-		local snapshot = manifest:snapshot()
-		local container = {}
-
-		snapshot:destroyed(container)
-
-		it("should serialize all of the destroyed entities destroyed", function()
-			   for _, entity in ipairs(container[1]) do
-				local entityId = bit32.band(entity, ENTITYID_MASK)
-				local version = bit32.rshift(entity, ENTITYID_WIDTH)
-
-				-- entityId will be equal to the identifier in destEnts
-				-- i.e. the versions of the initial idenitifers are equal to 0
-				expect(destEnts[entityId]).to.be.ok()
-				expect(version).to.equal(1)
+		it("should serialize all of the entities", function()
+			for i, entity in ipairs(manifest.entities) do
+				expect(container[1][i]).to.be.ok()
+				expect(container[1][i]).to.equal(entity)
 			end
 		end)
 	end)
 
 	describe("Components", function()
-		local snapshot = manifest:snapshot()
 		local container = {}
 
-		snapshot:components(container, test1, test2)
+		manifest:snapshot():components(container, test1, test2)
 
 		it("should serialize components with their entities", function()
 			local idx = 1
 
 			for componentId = 1, 2 do
-				for i, entity in ipairs(container[idx]) do
+				for i, entity in ipairs(manifest:_getPool(componentId).internal) do
 					local component = comps[componentId][entity]
 					local serializedComponent = container[idx + 1][i]
 
