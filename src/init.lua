55f7209db756159c16d465808a9bf0ffdb53c402
kennethloeffler <kenneth.loeffler@outlook.com>
2020-07-27 22:31:37 -0700

    Add basic loader

--- src/init.lua
+++ src/init.lua
@@ -1,3 +1,14 @@
 local Manifest = require(script.Parent.ecs.Manifest)
 
-return Manifest
+return function(projectRoot)
+	local components = projectRoot:FindFirstChild("components")
+	local ecs = Manifest.new()
+
+	for _, instance in ipairs(components:GetChildren()) do
+		if instance:IsA("ModuleScript") then
+			require(instance)(ecs)
+		end
+	end
+
+	return ecs
+end
