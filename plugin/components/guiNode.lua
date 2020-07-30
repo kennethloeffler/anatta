4ddae1228b19534a593c6af87f29e303e806d65d
Kenneth Loeffler <kenneth.loeffler@outlook.com>
2020-07-29 17:49:22 -0700

    Remove old plugin stuff

--- plugin/components/guiNode.lua
+++
@@ -1,3 +0,0 @@
-return function(ecs)
-	return ecs:define("Frame", "guiNode")
-end
--- plugin/ecs.lua
+++
@@ -1,8 +0,0 @@
-local ecs = require(script.Parent.Parent.ecs).new()
-local components = script.Parent.components
-
-for _, moduleScript in ipairs(components:GetChildren()) do
-	require(moduleScript)(ecs)
-end
-
-return ecs
