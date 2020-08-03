f5f87fb4dcb326b9520888f703c656e631ef3bcc
kennethloeffler <kenneth.loeffler@outlook.com>
2020-08-02 18:50:53 -0700

    Make src/init.lua return Manifest.new

--- src/init.lua
+++ src/init.lua
@@ -1,14 +1,1 @@
-local Manifest = require(script.Manifest)
-
-return function(projectRoot)
-	local components = projectRoot:FindFirstChild("Components")
-	local ecs = Manifest.new()
-
-	for _, instance in ipairs(components:GetDescendants()) do
-		if instance:IsA("ModuleScript") then
-			require(instance)(ecs)
-		end
-	end
-
-	return ecs
-end
+return require(script.Manifest).new
