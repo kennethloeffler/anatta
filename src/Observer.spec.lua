a7a2f430264cc4337b5bca737e4d4a319e155a78
kennethloeffler <kenneth.loeffler@outlook.com>
2020-10-22 21:02:06 -0700

    Add another test case

--- src/Observer.spec.lua
+++ src/Observer.spec.lua
@@ -23,6 +23,9 @@ return function()
 
 			context.obsUpdated = Constraint.new(manifest):updated(context.comp1, context.comp2)
 				:observer()
+
+			context.obsAllUpdated = Constraint.new(manifest):all(context.comp1, context.comp2)
+				:updated(context.comp3, context.comp4):observer()
 		end)
 
 		describe("all", function()
@@ -104,6 +107,42 @@ return function()
 
 				context.manifest:remove(entity, context.comp1)
 				expect(context.manifest:has(entity, context.obsUpdated)).to.equal(false)
+			end)
+		end)
+
+		describe("all updated", function()
+			it("should capture entities with all the required components and all the updated components", function(context)
+				local entity = context.manifest:create()
+
+				context.manifest:add(entity, context.comp1)
+				context.manifest:add(entity, context.comp2)
+
+				expect(context.manifest:has(entity, context.obsAllUpdated)).to.equal(false)
+
+				context.manifest:add(entity, context.comp3)
+				context.manifest:add(entity, context.comp4)
+
+				expect(context.manifest:has(entity, context.obsAllUpdated)).to.equal(false)
+
+				context.manifest:replace(entity, context.comp3)
+				context.manifest:replace(entity, context.comp4)
+
+				expect(context.manifest:has(entity, context.obsAllUpdated)).to.equal(true)
+			end)
+
+			it("should cull entities that lose any of the components", function(context)
+				local entity = context.manifest:create()
+
+				context.manifest:add(entity, context.comp1)
+				context.manifest:add(entity, context.comp2)
+				context.manifest:add(entity, context.comp3)
+				context.manifest:add(entity, context.comp4)
+				context.manifest:replace(entity, context.comp3)
+				context.manifest:replace(entity, context.comp4)
+
+				context.manifest:remove(entity, context.comp4)
+
+				expect(context.manifest:has(entity, context.obsAllUpdated)).to.equal(false)
 			end)
 		end)
 	end)
