0bcb798422b15cabe19776f66744451ad002ace6
kennethloeffler <kenneth.loeffler@outlook.com>
2020-12-22 08:17:00 -0800

    Make Entity module and add types

---
+++ src/Entity/init.lua
@@ -0,0 +1,9 @@
+local Reducer = require(script.Reducer)
+local Registry = require(script.Registry)
+local Selector = require(script.Selector)
+
+return {
+	Reducer = Reducer,
+	Registry = Registry,
+	Selector = Selector,
+}
