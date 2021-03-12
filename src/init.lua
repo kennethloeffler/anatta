4bb0e18061cbe1a31467fac4dc75593da3c938cc
Kenneth Loeffler <kenneth.loeffler@outlook.com>
2021-03-12 04:55:08 -0800

    Clean up

--- src/init.lua
+++ src/init.lua
@@ -1,34 +1,13 @@
 local Entity = require(script.Entity)
 
-local detach = Entity.Attachments.detach
-
-local System = {}
-System.__index = {}
-
-function System:on(event, callback)
-	table.insert(self._connections, event:Connect(callback))
-end
-
-function System:match()
-	local matcher = Entity.Matcher.new(self._registry)
-
-	table.insert(self._matchers, matcher)
-
-	return matcher
-end
-
-function System:getRegistry()
-	return self._registry
-end
-
 local Anatta = {}
 Anatta.__index = Anatta
 
-function Anatta.new(components)
+function Anatta.define(components)
 	return setmetatable({
 		_registry = Entity.Registry.new(components),
 		_systems = {},
-	})
+	}, Anatta)
 end
 
 function Anatta:loadSystems(container)
@@ -37,35 +16,21 @@ function Anatta:loadSystems(container)
 			descendant:IsA("ModuleScript")
 			and not descendant.Name:match("%.spec$")
 		then
-			Anatta:_loadSystem(descendant)
 		end
 	end
 end
 
 function Anatta:_loadSystem(moduleScript)
 	local loadSystem = require(moduleScript)
-	local system = setmetatable({
-		_connections = self._connections,
-		_matchers = self._matchers,
-		_registry = self._registry,
-	}, self._registry)
+	local system = Entity.System.new(self._registry)
 
-	loadSystem(system)
 	self._systems[moduleScript] = system
+
+	return loadSystem(system)
 end
 
 function Anatta:_unloadSystem(moduleScript)
-	local system = self._systems[moduleScript]
-
-	for _, connection in ipairs(system._connections) do
-		connection:Disconnect()
-	end
-
-	for _, matcher in ipairs(system._matchers) do
-		detach(matcher.collection)
-	end
-
-	self._systems[moduleScript] = nil
+	self._systems[moduleScript]:unload()
 end
 
 return Anatta
