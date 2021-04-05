07571bdf24287827b6612eadfa5c75622a16fa8e
Kenneth Loeffler <kenneth.loeffler@outlook.com>
2021-04-05 16:37:02 -0700

    Type introspection

---
+++ src/Core/Type.lua
@@ -0,0 +1,128 @@
+--[[
+
+	Wrapper around t that lets us inspect detailed type information after the
+	fact.
+
+]]
+
+local t = require(script.Parent.Parent.t)
+
+local firstOrder = {
+	boolean = true,
+	none = true,
+	string = true,
+	table = true,
+	number = true,
+	nan = true,
+	integer = true,
+	Axes = true,
+	BrickColor = true,
+	CFrame = true,
+	Color3 = true,
+	ColorSequence = true,
+	ColorSequenceKeypoint = true,
+	DockWidgetPluginGuiInfo = true,
+	Faces = true,
+	Instance = true,
+	NumberRange = true,
+	NumberSequence = true,
+	NumberSequenceKeypoint = true,
+	PathWaypoint = true,
+	PhysicalProperties = true,
+	Random = true,
+	Ray = true,
+	Rect = true,
+	Region3 = true,
+	Region3int16 = true,
+	TweenInfo = true,
+	UDim = true,
+	UDim2 = true,
+	Vector2 = true,
+	Vector3 = true,
+	Vector3int16 = true,
+	Enum = true,
+	EnumItem = true,
+	numberPositive = true,
+	numberNegative = true,
+}
+
+local secondOrder = {
+	literal = true,
+	keyOf = true,
+	valueOf = true,
+	integer = true,
+	numberMin = true,
+	numberMax = true,
+	numberMinExclusive = true,
+	numberMaxExclusive = true,
+	numberConstrained = true,
+	numberConstrainedExclusive = true,
+	match = true,
+	keys = true,
+	values = true,
+	map = true,
+	set = true,
+	array = true,
+	strictArray = true,
+	union = true,
+	some  = true,
+	intersection = true,
+	every  = true,
+	interface = true,
+	strictInterface = true,
+	instanceOf = true,
+	instanceIsA = true,
+	enum = true,
+	wrap = true,
+	strict = true,
+}
+
+local concrete = {
+	match = "string",
+	numberMin = "number",
+	numberMax = "number",
+	numberMinExclusive = "number",
+	numberMaxExclusive =  "number",
+	numberConstrained = "number",
+	numberConstrainedExclusive = "number",
+	numberPositive = "number",
+	numberNegative = "number",
+}
+
+local function getConcrete(typeName)
+	return
+		concrete[typeName]
+		or (firstOrder[typeName] and typeName)
+end
+
+local function unwrap(...)
+	local unwrapped = table.create(select("#", ...))
+
+	for i = 1, select("#", ...) do
+		unwrapped[i] = select(i, ...).check or select(i, ...)
+	end
+
+	return unpack(unwrapped)
+end
+
+local Type = {}
+
+for typeName in pairs(t) do
+	if firstOrder[typeName] ~= nil then
+		Type[typeName] = {
+			check = t[typeName],
+			name = typeName,
+			concrete = getConcrete(typeName),
+		}
+	elseif secondOrder[typeName] ~= nil then
+		Type[typeName] = function(...)
+			return {
+				args = { ... },
+				check = t[typeName](unwrap(...)),
+				name = typeName,
+			}
+		end
+	end
+end
+
+return Type
