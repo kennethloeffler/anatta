646a80d3a1b3748a58ae225ca466c6f83259b7a1
Kenneth Loeffler <kenneth.loeffler@outlook.com>
2021-04-05 16:44:55 -0700

    Remove FOCUS

--- src/Core/Type.spec.lua
+++ src/Core/Type.spec.lua
@@ -2,8 +2,6 @@ return function()
 	local Type = require(script.Parent.Type)
 	local t = require(script.Parent.Parent.t)
 
-	FOCUS()
-
 	it("should return a primitive definition for first-order functions", function()
 		local ty = Type.Vector3
 
