3d98c64923d0e3d4a8dd17467100fcd3f043e2c1
Kenneth Loeffler <kenneth.loeffler@outlook.com>
2021-07-02 17:38:52 -0700

    Add getValidEntity

---
+++ plugin/src/getValidEntity.lua
@@ -0,0 +1,24 @@
+local Constants = require(script.Parent.Constants)
+
+local ENTITY_ATTRIBUTE_NAME = Constants.EntityAttributeName
+
+return function(registry, instance)
+	local entity = instance:GetAttribute(ENTITY_ATTRIBUTE_NAME)
+
+	assert(entity == nil or typeof(entity) == "number")
+
+	if
+		entity == nil
+		or (registry:valid(entity) and registry:get(entity, "__anattaAssociatedInstance") ~= instance)
+		or not registry:valid(entity)
+	then
+		local newEntity = registry:create()
+
+		instance:SetAttribute(ENTITY_ATTRIBUTE_NAME, newEntity)
+		registry:add(newEntity, "__anattaAssociatedInstance", instance)
+
+		return newEntity
+	else
+		return entity
+	end
+end
