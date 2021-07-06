88f65b88bf47fd775cd5aaf8b6a834ad2c761674
Kenneth Loeffler <kenneth.loeffler@outlook.com>
2021-07-05 19:14:42 -0700

    Delete getAttributeChecks and getAttributeDefault

--- lib/src/util/init.lua
+++ lib/src/util/init.lua
@@ -1,10 +1,8 @@
-local getAttributeChecks = require(script.getAttributeChecks)
 local jumpAssert = require(script.jumpAssert)
 local tryFromAttribute = require(script.tryFromAttribute)
 local tryToAttribute = require(script.tryToAttribute)
 
 return {
-	getAttributeChecks = getAttributeChecks,
 	jumpAssert = jumpAssert,
 	tryFromAttribute = tryFromAttribute,
 	tryToAttribute = tryToAttribute,
