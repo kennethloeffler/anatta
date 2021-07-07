bfbe7ef66de7e1ac70778f261e7dff4fc034cc6b
Kenneth Loeffler <kenneth.loeffler@outlook.com>
2021-07-07 05:07:16 -0700

    Fix SinglePureCollection:each and add SinglePureCollection:update
    This makes it match PureCollection's API (as it should).

--- lib/src/Entity/SinglePureCollection.lua
+++ lib/src/Entity/SinglePureCollection.lua
@@ -7,11 +7,26 @@ function SinglePureCollection.new(pool)
 	}, SinglePureCollection)
 end
 
+function SinglePureCollection:update(callback)
+	local components = self._pool.components
+	local updated = self._pool.updated
+
+	for i, entity in ipairs(self._pool.dense) do
+		local oldComponent = components[i]
+		local newComponent = callback(entity, oldComponent)
+
+		if newComponent ~= oldComponent then
+			updated:dispatch(entity, newComponent)
+			components[i] = newComponent
+		end
+	end
+end
+
 function SinglePureCollection:each(callback)
 	local components = self._pool.components
 
 	for i, entity in ipairs(self._pool.dense) do
-		components[i] = callback(entity, components[i])
+		callback(entity, components[i])
 	end
 end
 
