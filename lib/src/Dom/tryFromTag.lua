b2f7775c6c0f30e5d0a494a5506e7bef4c0ba765
Kenneth Loeffler <kenneth.loeffler@outlook.com>
2021-07-08 22:37:37 -0700

    Add comment

--- lib/src/Dom/tryFromTag.lua
+++ lib/src/Dom/tryFromTag.lua
@@ -6,6 +6,13 @@ local tryFromAttribute = require(script.Parent.tryFromAttribute)
 
 local ENTITY_ATTRIBUTE_NAME = Constants.EntityAttributeName
 
+-- Populates a pool with attribute data from the Roblox DataModel by getting all instances
+-- tagged with the pool's name and attempting to convert their attributes into entities
+-- and components of the pool's type.
+
+-- If a component conversion fails, the entire function fails. However, it is not a hard
+-- failure for an entity attribute to be invalid; if an entity attribute is invalid, the
+-- function skips it and prints a warning.
 return function(pool)
 	local componentName = pool.name
 	local typeDefinition = pool.typeDefinition
