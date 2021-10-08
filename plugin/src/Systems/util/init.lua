64bbe62e0a156d28144aa20f4382be0b210ad42b
Kenneth Loeffler <kenneth.loeffler@outlook.com>
2021-10-08 13:58:42 -0700

    Delete plugin
    Work on the plugin continues on the roided-tag-editor branch

--- plugin/src/Systems/util/init.lua
+++
@@ -1,7 +0,0 @@
-local getValidEntity = require(script.getValidEntity)
-local tryGetValidEntityAttribute = require(script.tryGetValidEntityAttribute)
-
-return {
-	getValidEntity = getValidEntity,
-	tryGetValidEntityAttribute = tryGetValidEntityAttribute,
-}
