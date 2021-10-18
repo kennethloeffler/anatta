2800f431c08a7a42fc1d8d6ea646393cceea1222
Kenneth Loeffler <kenneth.loeffler@outlook.com>
2021-10-17 19:56:43 -0700

    Allow functions to be given in the withAttachments return table

--- lib/src/Core/Finalizers.lua
+++ lib/src/Core/Finalizers.lua
@@ -2,7 +2,12 @@ local function disconnect(connection)
 	connection:Disconnect()
 end
 
+local function call(func)
+	func()
+end
+
 return {
+	["function"] = call,
 	RBXScriptConnection = disconnect,
 	table = disconnect,
 	Instance = game.Destroy,
