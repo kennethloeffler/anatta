776af82a783a3e8136097ec02c17ffb1c5ac454b
LouieK22 <louie@tinybits.me>
2021-10-19 17:52:39 -0500

    welcome the boolean

---
+++ plugin/src/Components/Properties/BaseProperty.story.lua
@@ -0,0 +1,30 @@
+local Vendor = script.Parent.Parent.Parent.Parent
+local Roact = require(Vendor.Roact)
+
+local BaseProperty = require(script.Parent.BaseProperty)
+
+return function(target)
+	local element = Roact.createFragment({
+		Layout = Roact.createElement("UIListLayout", {
+			Padding = UDim.new(0, 5),
+			SortOrder = Enum.SortOrder.LayoutOrder,
+			FillDirection = Enum.FillDirection.Vertical,
+			HorizontalAlignment = Enum.HorizontalAlignment.Center,
+			VerticalAlignment = Enum.VerticalAlignment.Center,
+		}),
+		Container = Roact.createElement("Frame", {
+			LayoutOrder = 0,
+			Size = UDim2.fromOffset(300, 25),
+			BackgroundTransparency = 1,
+		}, {
+			BaseProperty = Roact.createElement(BaseProperty, {
+				Text = "Hello World"
+			})
+		}),
+	})
+
+	local handle = Roact.mount(element, target)
+	return function()
+		Roact.unmount(handle)
+	end
+end
