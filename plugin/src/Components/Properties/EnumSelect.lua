369dee34d84ca018afdb771e1d39ce96af8a7705
LouieK22 <louie@tinybits.me>
2021-10-20 12:35:39 -0500

    Enum support

---
+++ plugin/src/Components/Properties/EnumSelect.lua
@@ -0,0 +1,34 @@
+local Modules = script.Parent.Parent.Parent.Parent
+local Roact = require(Modules.Roact)
+local StudioComponents = require(Modules.StudioComponents)
+
+local BaseProperty = require(script.Parent.BaseProperty)
+
+local function EnumItem(props)
+	local items = {}
+
+	for _, enum in ipairs(Enum:GetEnums()) do
+		table.insert(items, tostring(enum))
+	end
+
+	return Roact.createElement(BaseProperty, {
+		Text = props.Key
+	}, {
+		Centered = Roact.createElement("Frame", {
+			Size = UDim2.new(1, 0, 0, 15),
+			BackgroundTransparency = 1,
+			AnchorPoint = Vector2.new(0, 0.5),
+			Position = UDim2.new(0, 0, 0.5, 0),
+		}, {
+			Dropdown = Roact.createElement(StudioComponents.Dropdown, {
+				Items = items,
+				Item = tostring(props.Selected),
+				OnSelected = function(enumName)
+					props.OnSelected(Enum[enumName])
+				end,
+			})
+		})
+	})
+end
+
+return EnumItem
---
+++ plugin/src/Components/Properties/EnumSelect.story.lua
@@ -0,0 +1,47 @@
+local Vendor = script.Parent.Parent.Parent.Parent
+local Roact = require(Vendor.Roact)
+
+local EnumSelect = require(script.Parent.EnumSelect)
+
+local Wrapper = Roact.Component:extend("CheckboxWrapper")
+
+function Wrapper:init()
+	self:setState({
+		Selected = Enum.Font,
+	})
+end
+
+function Wrapper:render()
+	return Roact.createFragment({
+		Layout = Roact.createElement("UIListLayout", {
+			Padding = UDim.new(0, 5),
+			FillDirection = Enum.FillDirection.Vertical,
+			HorizontalAlignment = Enum.HorizontalAlignment.Center,
+			VerticalAlignment = Enum.VerticalAlignment.Center,
+			SortOrder = Enum.SortOrder.Name,
+		}),
+		Container = Roact.createElement("Frame", {
+			LayoutOrder = 0,
+			Size = UDim2.fromOffset(300, 25),
+			BackgroundTransparency = 1,
+		}, {
+			EnumSelect = Roact.createElement(EnumSelect, {
+				Key = "Pick Yo Enum",
+				Selected = self.state.Selected,
+				OnSelected = function(enum)
+					self:setState({
+						Selected = enum
+					})
+				end,
+			})
+		}),
+	})
+end
+
+return function(target)
+	local element = Roact.createElement(Wrapper)
+	local handle = Roact.mount(element, target)
+	return function()
+		Roact.unmount(handle)
+	end
+end
