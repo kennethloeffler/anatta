7e6ebd4ff1c6d229f5afe197ada240df93414648
Kenneth Loeffler <kenloef@gmail.com>
2022-01-24 13:30:12 -0800

    Fix table finalizer

--- lib/src/Core/Finalizers.lua
+++ lib/src/Core/Finalizers.lua
@@ -1,6 +1,15 @@
-local function disconnect(connection)
-	connection:Disconnect()
+local function disconnectOrDestroy(arg)
+	if typeof(arg.Disconnect) == "function" then
+		arg:Disconnect()
+	elseif arg.Destroy == "function" then
+		arg:Destroy()
+	end
 end
+
+local dummyConnection = game.Changed:Connect(function()
+end)
+
+dummyConnection:Disconnect()
 
 local function call(func)
 	func()
@@ -8,7 +17,7 @@ end
 
 return {
 	["function"] = call,
-	RBXScriptConnection = disconnect,
-	table = disconnect,
+	RBXScriptConnection = dummyConnection.Disconnect,
+	table = disconnectOrDestroy,
 	Instance = game.Destroy,
 }
