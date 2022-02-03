7505ee4697600c6b5b4f6ba7da6bd922b75dadd4
Kenneth Loeffler <kenneth.loeffler@outlook.com>
2022-02-03 11:38:56 -0800

    Don't throw in entityIsValid

--- lib/src/World/Registry.lua
+++ lib/src/World/Registry.lua
@@ -421,9 +421,13 @@ end
 	@return boolean
 ]=]
 function Registry:entityIsValid(entity)
-	jumpAssert(typeof(entity) == "number", ErrEntityNotANumber, entity)
+	if typeof(entity) ~= "number" then
+		return false, ErrEntityNotANumber:format(entity)
+	elseif self._entities[bit32.band(entity, ENTITYID_MASK)] ~= entity then
+		return false, ErrInvalidEntity:format(entity)
+	end
 
-	return self._entities[bit32.band(entity, ENTITYID_MASK)] == entity
+	return true, ""
 end
 
 --[=[
