03e821fa5eb2a2b4834dbe116182f9d5139ffe6c
Kenneth Loeffler <kenloef@gmail.com>
2022-02-28 18:07:13 -0800

    Get ComponentValues started

--- plugin/src/Components/ComponentList/ComponentValues/init.lua
+++ plugin/src/Components/ComponentList/ComponentValues/init.lua
@@ -1,32 +1,80 @@
 local Modules = script.Parent.Parent.Parent.Parent
+local Properties = script.Parent.Parent.Properties
 local Roact = require(Modules.Roact)
 local RoactRodux = require(Modules.RoactRodux)
+local Llama = require(Modules.Llama)
 local Actions = require(Modules.Plugin.Actions)
-local ComponentManager = require(Modules.Plugin.ComponentManager)
+local Anatta = require(Modules.Anatta)
 local PluginGlobals = require(Modules.Plugin.PluginGlobals)
 
+local InstanceSelect = require(Properties.InstanceSelect)
+local NumberInput = require(Properties.NumberInput)
 local VerticalExpandingList = require(Modules.StudioComponents.VerticalExpandingList)
 
 local ComponentValues = Roact.Component:extend("ComponentValues")
 
-local function createComponentMembers(type)
-	if typeof(type) == "table" then
-		for _, v in pairs(type) do
-			createComponentMembers(v)
-		end
-	elseif typeof(type) == "number" then
+local function createInstanceElement(name, typeDefinition, value)
+	local typeParam = typeDefinition.typeParams[1]
+
+	return Roact.createElement(InstanceSelect, {
+		Key = name,
+		Instance = value,
+		IsA = typeDefinition.typeName == "instanceIsA" and typeParam,
+		ClassName = typeDefinition.typeName == "instanceOf" and typeParam,
+
+		OnChanged = function() end,
+	})
+end
+
+local Types = {
+	instance = createInstanceElement,
+	Instance = createInstanceElement,
+	instanceIsA = createInstanceElement,
+	instanceOf = createInstanceElement,
+	number = function(name, typeDefinition, value)
+		return Roact.createElement(NumberInput, { Key = name, Value = value, OnChanged = function() end })
+	end,
+}
+
+local function createComponentMembers(name, typeDefinition, value, members)
+	local typeOk, concreteType = typeDefinition:tryGetConcreteType()
+
+	if not typeOk then
+		warn(concreteType)
+		return members
 	end
+
+	if typeof(concreteType) == "table" then
+		local typeParams
+
+		if typeDefinition.typeName == "strictArray" then
+			typeParams = typeDefinition.typeParams
+		else
+			typeParams = typeDefinition.typeParams[1]
+		end
+
+		for fieldName in pairs(concreteType) do
+			createComponentMembers(fieldName, typeParams[fieldName], value[fieldName], members)
+		end
+	elseif Types[concreteType] ~= nil then
+		local element = Types[concreteType](name, typeDefinition, value)
+		table.insert(members, element)
+	end
+
+	return members
 end
 
 function ComponentValues:render()
 	local props = self.props
-	local typeOk, type = props.componentMenu.Definition.type:tryGetConcreteType()
+	local componentDefinition = props.Definition
+	local name = componentDefinition.name
+	local typeDefinition = componentDefinition.pluginType and componentDefinition.pluginType or componentDefinition.type
 
-	if not typeOk then
-		warn(type)
-	end
-
-	return Roact.createElement(VerticalExpandingList, {}, createComponentMembers(type))
+	return Roact.createElement(
+		VerticalExpandingList,
+		{},
+		createComponentMembers(name, typeDefinition, props.Values[1], {})
+	)
 end
 
 local function mapStateToProps(state)
