042df976ad99b0a834d7128a5e4f4cfd0c6ecbcd
Kenneth Loeffler <kenloef@gmail.com>
2022-02-28 18:06:43 -0800

    Remove border on BaseProperty value

--- plugin/src/Components/Properties/BaseProperty.lua
+++ plugin/src/Components/Properties/BaseProperty.lua
@@ -11,24 +11,24 @@ local function BaseProperty(props)
 			Size = UDim2.new(1, 0, 0, 25),
 		}, {
 			UIPadding = Roact.createElement("UIPadding", {
-				PaddingLeft = UDim.new(0, 25)
+				PaddingLeft = UDim.new(0, 25),
 			}),
 			Key = Roact.createElement("TextLabel", {
 				BackgroundTransparency = 1,
 				Size = UDim2.new(0.5, 0, 1, 0),
 				Text = props.Text,
 				TextColor3 = theme:GetColor(Enum.StudioStyleGuideColor.MainText),
-				TextXAlignment = Enum.TextXAlignment.Left
+				TextXAlignment = Enum.TextXAlignment.Left,
 			}),
 			ValueContainer = Roact.createElement("Frame", {
 				BackgroundColor3 = theme:GetColor(Enum.StudioStyleGuideColor.MainBackground),
-				BorderSizePixel = 1,
+				BorderSizePixel = 0,
 				BorderColor3 = theme:GetColor(Enum.StudioStyleGuideColor.Border),
 				Size = UDim2.new(0.5, 0, 1, 0),
-				Position = UDim2.new(0.5, 0, 0, 0)
+				Position = UDim2.new(0.5, 0, 0, 0),
 			}, {
-				Value = Roact.oneChild(props[Roact.Children])
-			})
+				Value = Roact.oneChild(props[Roact.Children]),
+			}),
 		})
 	end)
 end
